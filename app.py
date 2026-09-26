0a85616afa089980f2b62c3793a9c9af079267d0
Aafiya Anis <aafiyaanis43@gmail.comL>
2026-09-26 11:50:08 +0530

    feat: show running commit ID in footer

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///crisislens.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+@app.context_processor
+def inject_commit_id():
+    commit_id = os.getenv("RENDER_GIT_COMMIT", "local")
+    return {"commit_id": commit_id[:7]}
 
 
 class Crisis(db.Model):
